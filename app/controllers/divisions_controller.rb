623b83aca7dba28625057a1573d596235028cac7
Rebecca Appleyard <rklappleyard@gmail.com>
2016-06-09 14:38:03 +0100

    Show method in for division

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -19,5 +19,10 @@ class DivisionsController < ApplicationController
 		person_uri = resource_uri(params[:person_id])
 		@person = Person.find(person_uri)
 		@divisions = Division.find_by_person(person_uri)
+
+	def show
+		division_uri = resource_uri(params[:id])
+		p division_uri
+		@division = Division.find(division_uri)
 	end
 end
