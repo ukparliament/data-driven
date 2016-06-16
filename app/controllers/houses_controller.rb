5195218ee476dc736bc0f87fe0acf769dbe59752
Rebecca Appleyard <rklappleyard@gmail.com>
2016-06-16 11:16:49 +0100

    Refactored house class to remove tripod

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,6 @@
 class HousesController < ApplicationController
 	def index 
-		@houses = House.all.resources
+		@houses = House.all
 
 		format(@houses)
 	end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,12 +1,20 @@
 class House
-	include Tripod::Resource
-	
-	rdf_type 'http://data.parliament.uk/schema/parl#House'
 
-	field :label, 'http://www.w3.org/2000/01/rdf-schema#label'
-
-	def id
-		self.uri.to_s.split("/").last
+	def self.all
+		client = SPARQL::Client.new(DataDriven::Application.config.database)
+		result = client.query("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+							   PREFIX schema: <http://schema.org/>
+								select ?house ?name where { 
+								?house rdf:type <http://data.parliament.uk/schema/parl#House>;
+    	   								<http://www.w3.org/2000/01/rdf-schema#label> ?name .
+								}")
+		result.map do |solution| 
+			Hashit.new(
+			{
+				:id => solution.house.to_s.split("/").last,
+				:label => solution.name.to_s
+			})
+		end
 	end
 
 end
