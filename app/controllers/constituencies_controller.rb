b47d7a2e1a185b3e200eee83250c2fc28ff57f9f
giusepped <giusdesan@gmail.com>
2016-07-13 15:08:01 +0100

    redirecting constituencies to python app

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -1,12 +1,22 @@
 class ConstituenciesController < ApplicationController
 
   def index
+    if(request.format.html?)
+      redirect_to('http://petitions-data-driven.ci.ukpds.org/constituencies')
+      return ''
+    end
+
     data = Constituency.all
 
     format(data)
   end
 
   def show
+    if(request.format.html?)
+      redirect_to("http://petitions-data-driven.ci.ukpds.org/constituencies/#{params[:id]}")
+      return ''
+    end
+
     constituency_uri = resource_uri(params[:id])
     data = Constituency.find(constituency_uri)
 
