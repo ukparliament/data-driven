74f28138f7f6c4894e223a9e19d2192bfedc3787
Rebecca Appleyard <rklappleyard@gmail.com>
2016-07-21 16:57:17 +0100

    Finished view for business items

--- app/controllers/business_items_controller.rb
+++ app/controllers/business_items_controller.rb
@@ -1,11 +1,15 @@
 class BusinessItemsController < ApplicationController
 	def index
-		date = params[:date]
-		@order_paper = OrderPaperItem.all(date)
+		date = params[:id]
+		data = BusinessItem.all(date)
+		@order_paper = data[:hierarchy]
 
 		@json_ld = json_ld(data)
 		format(data)
 	end
 
-	#rename this method
+	def show
+		
+	end
+
 end
