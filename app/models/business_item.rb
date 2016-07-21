0d08e1c8e4d31a873a7e5ea5abc6a3b211056762
Rebecca Appleyard <rklappleyard@gmail.com>
2016-07-21 17:12:55 +0100

    Added query for business item show

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -32,4 +32,46 @@ class BusinessItem < QueryObject
 
 		{ :graph => result, :hierarchy => hierarchy }
 	end
+
+	def self.find(uri)
+		result = self.query("
+			PREFIX parl: <http://data.parliament.uk/schema/parl#>
+			PREFIX dcterms: <http://purl.org/dc/terms/>
+			PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+			PREFIX schema: <http://schema.org/>
+			CONSTRUCT {
+			    ?item
+			        dcterms:date ?date ;
+			    	dcterms:title ?title ;
+        			dcterms:identifier ?identifier ;
+        			parl:member ?person ;
+            		dcterms:abstract ?abstract ;
+            		schema:previousItem ?previousItem .
+			}
+			WHERE { 
+    			SELECT ?item ?date ?title ?identifier ?person ?abstract ?previousItem 
+    			WHERE {
+        				?item
+			        		a parl:OrderPaperItem ;
+			        		dcterms:date ?date ;
+			    			dcterms:title ?title ;
+        					dcterms:identifier ?identifier .
+
+        	  	OPTIONAL {
+        				?item
+            				parl:member ?person ;
+        		}
+        		OPTIONAL {
+            			?item
+            				dcterms:abstract ?abstract ;
+        		}
+            	OPTIONAL {
+            			?item
+                        	schema:previousItem ?previousItem ;
+        		}	
+            		
+         FILTER(?item = <http://id.ukpds.org/13e1ab8d-4158-4006-b9b7-b4719da8aac4>)
+    }      
+}")
+	end
 end
