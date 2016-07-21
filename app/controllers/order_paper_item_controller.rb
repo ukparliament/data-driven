878207c779665b76c826d1d09a0b571c73de3e1d
Rebecca Appleyard <rklappleyard@gmail.com>
2016-07-21 11:57:09 +0100

    Set up for order paper items

---
+++ app/controllers/order_paper_item_controller.rb
@@ -0,0 +1,9 @@
+class OrderPaperItemController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+	
+end
---
+++ app/models/order_paper_item.rb
@@ -0,0 +1,5 @@
+class OrderPaperItem < QueryObject
+	include Vocabulary
+
+
+end
