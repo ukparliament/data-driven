3a1d6671fbc52c182e27fe07a9555667d270528c
giusepped <giusdesan@gmail.com>
2016-07-21 14:13:02 +0100

    started order paper item query

--- app/models/order_paper_item.rb
+++ app/models/order_paper_item.rb
@@ -1,5 +1,26 @@
 class OrderPaperItem < QueryObject
 	include Vocabulary
 
-
+	def self.all_dates
+		result = self.query('
+			PREFIX parl: <http://data.parliament.uk/schema/parl#>
+			PREFIX dcterms: <http://purl.org/dc/terms/>
+			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			CONSTRUCT
+			{
+			    _:x parl:date ?date .
+			    _:x parl:count ?count .
+			}
+			WHERE {
+			    SELECT ?date (COUNT(?s) AS ?count)
+			    WHERE 
+			        {
+			            ?s rdf:type parl:OrderPaperItem ;
+			               dcterms:date ?date .
+			        }
+			    GROUP BY ?date
+			    ORDER BY ?date
+			}
+		')
+	end
 end
