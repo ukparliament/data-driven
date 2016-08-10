01c60be49af4d0ce9545053a1da897ad8de8738e
Rebecca Appleyard <rklappleyard@gmail.com>
2016-08-10 14:59:06 +0100

    Refactored votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,61 +4,49 @@ class Vote < QueryObject
 	def self.find_by_division(division_uri)
 	result = self.query("
 		PREFIX parl: <http://data.parliament.uk/schema/parl#>
-      	PREFIX dcterms: <http://purl.org/dc/terms/>
-      	PREFIX schema: <http://schema.org/>
-      	CONSTRUCT {
-      		<#{division_uri}>
-      			dcterms:title ?title .
-      		?person
-            a schema:Person ;
-      			schema:name ?name ;
-      			parl:voteValue ?value .
-      	}
-      	WHERE {
-      		?vote 
-      			parl:division <#{division_uri}> ;
-      			parl:value ?value ;
-      			parl:member ?person .
-      		<#{division_uri}>
-      			dcterms:title ?title .
-      		?person
-      			schema:name ?name .
-      	}
-		")
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX schema: <http://schema.org/>
+    CONSTRUCT {
+    	?division
+    		dcterms:title ?title .
+    	?person
+    		schema:name ?name ;
+    		parl:voteValue ?value .
+    }
+    WHERE {
+    	?vote 
+    		parl:division ?division ;
+    		parl:value ?value ;
+    		parl:member ?person .
+    	?division
+    		dcterms:title ?title .
+    	?person
+    		schema:name ?name .
+      FILTER(?division = <#{division_uri}>)
+    }
+	")
 
-	division_title_pattern = RDF::Query::Pattern.new(
-		RDF::URI.new(division_uri),
-		Dcterms.title,
-		:title)
+	title = self.get_object(result, RDF::URI.new(division_uri), Dcterms.title).to_s
 
-	id = self.get_id(division_uri)
-	title = result.first_literal(division_title_pattern)
+  members_pattern = RDF::Query::Pattern.new(
+    :person,
+    Schema.name,
+    :name)
 
-    members = result.subjects
-    	.select{ |subject| subject != RDF::URI.new(division_uri) }
-    	.map do |subject|
-    		name_pattern = RDF::Query::Pattern.new(
-			subject,
-			Schema.name,
-			:name)
-    		vote_value_pattern = RDF::Query::Pattern.new(
-			subject,
-			Parl.voteValue,
-			:voteValue)
-	
-			name = result.first_literal(name_pattern)
-			vote_value = result.first_object(vote_value_pattern)
+  members = result.query(members_pattern).subjects.map do |subject|
+    name = self.get_object(result, subject, Schema.name).to_s
+		vote_value = self.get_object(result, subject, Parl.voteValue).to_s
 
-			{
-    			:id => self.get_id(subject),
-    			:name => name.to_s,
-    			:vote_value => vote_value.to_s
-    		}
-		end
+		{
+    	:id => self.get_id(subject),
+    	:name => name,
+    	:vote_value => vote_value
+    }
+	end
 
     hierarchy = {
-    	:id => id,
-    	:title => title.to_s,
+    	:id => self.get_id(division_uri),
+    	:title => title,
     	:members => members
     }
 
@@ -67,66 +55,51 @@ class Vote < QueryObject
 	end	
 
 	def self.find_by_person(person_uri)
-	result = self.query("
-		PREFIX parl: <http://data.parliament.uk/schema/parl#>
-      	PREFIX dcterms: <http://purl.org/dc/terms/>
-      	PREFIX schema: <http://schema.org/>
-      	CONSTRUCT {
-      		?person
-            a schema:Person ;
-      			schema:name ?name .
-      		?division
-      			dcterms:title ?title ;
-      			parl:voteValue ?value .
-      	}
-      	WHERE {
-          ?person schema:name ?name .
-
-          OPTIONAL {
-            ?vote 
-              parl:member ?person ;
-              parl:value ?value ;
-              parl:division ?division .
-            ?division
-              dcterms:title ?title .
-          }
-          FILTER(?person = <#{person_uri}>)
-      	}
-		")
-
-	member_name_pattern = RDF::Query::Pattern.new(
-		RDF::URI.new(person_uri),
-		Schema.name,
-		:name)
-
-	id = self.get_id(person_uri)
-	name = result.first_literal(member_name_pattern)
-
-    divisions = result.subjects
-    	.select{ |subject| subject != RDF::URI.new(person_uri) }
-    	.map do |subject|
-    		title_pattern = RDF::Query::Pattern.new(
-			subject,
-			Dcterms.title,
-			:title)
-    		vote_value_pattern = RDF::Query::Pattern.new(
-			subject,
-			Parl.voteValue,
-			:voteValue)
-	
-			title = result.first_literal(title_pattern)
-			vote_value = result.first_object(vote_value_pattern)
-			p vote_value
-			{
-    			:id => self.get_id(subject),
-    			:title => title.to_s,
-    			:vote_value => vote_value.to_s
-    		}
-		end
+	  result = self.query("
+	 	  PREFIX parl: <http://data.parliament.uk/schema/parl#>
+      PREFIX dcterms: <http://purl.org/dc/terms/>
+      PREFIX schema: <http://schema.org/>
+      CONSTRUCT {
+      	?person
+          a schema:Person ;
+      		schema:name ?name .
+      	?division
+      		dcterms:title ?title ;
+      		parl:voteValue ?value .
+      }
+      WHERE {
+        ?person schema:name ?name
+        OPTIONAL {
+          ?vote 
+            parl:member ?person ;
+            parl:value ?value ;
+            parl:division ?division .
+          ?division
+            dcterms:title ?title .
+        }
+        FILTER(?person = <#{person_uri}>)
+      }
+	 	")
+  
+	  name = self.get_object(result, RDF::URI.new(person_uri), Schema.name).to_s
+    division_pattern = RDF::Query::Pattern.new(
+      :division,
+      Dcterms.title,
+      :title)
+    divisions = result.query(division_pattern).subjects.map do |subject|
+	 	  title = self.get_object(result, subject, Dcterms.title).to_s
+	 	  vote_value = self.get_object(result, subject, Parl.voteValue).to_s
+  
+	 		{
+      	:id => self.get_id(subject),
+      	:title => title,
+      	:vote_value => vote_value
+      }
+	  end
 
     hierarchy = {
-    	:id => id,
-    	:name => name.to_s,
+    	:id => self.get_id(person_uri),
+    	:name => name,
     	:divisions => divisions
     }
 
